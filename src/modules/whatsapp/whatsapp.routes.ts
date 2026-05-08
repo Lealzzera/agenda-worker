ea1db56d6b9f8c917ce867617bf70c2bba05e16b
Matheus <matheus.mleall@gmail.com>
2026-05-08 11:54:39 -0300

    added new endpoint to disconnect whatsapp

--- src/modules/whatsapp/whatsapp.routes.ts
+++ src/modules/whatsapp/whatsapp.routes.ts
@@ -1,9 +1,17 @@
 import { verifyJwt } from "@/middlewares/verify-jwt";
 import { FastifyInstance } from "fastify";
+import { disconnectController } from "./disconnect.controller";
 import { postQrCodeController } from "./qrCode.controller";
 
 export async function whatsappRoutes(app: FastifyInstance) {
   app.post("/qr-code", { preHandler: [verifyJwt] }, async (req, res) => {
     return postQrCodeController(req, res);
   });
+  app.delete(
+    "/disconnect/:sessionName",
+    { preHandler: [verifyJwt] },
+    async (req, res) => {
+      return disconnectController(req, res);
+    },
+  );
 }
