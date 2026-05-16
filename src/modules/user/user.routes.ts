b2621b5994add798fb3bc06ac57d55367e6ce2ab
Matheus <matheus.mleall@gmail.com>
2026-05-16 20:44:42 -0300

    new route to get the email

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,6 +1,29 @@
 import { FastifyInstance } from "fastify";
-import { registerUserController } from "./user.controller";
+import {
+  checkUserEmailController,
+  registerUserController,
+} from "./user.controller";
 
 export async function userRoutes(app: FastifyInstance) {
-    app.post('/register', {config: {rateLimit: {max: 5, timeWindow: '1 minute'}}}, async (req, res) => registerUserController(req, res))
+  app.post(
+    "/register",
+    { config: { rateLimit: { max: 5, timeWindow: "1 minute" } } },
+    async (req, res) => registerUserController(req, res),
+  );
+  app.get(
+    "/check-email",
+    {
+      config: { rateLimit: { max: 5, timeWindow: "1 minute" } },
+      schema: {
+        querystring: {
+          type: "object",
+          properties: {
+            email: { type: "string" },
+          },
+          required: ["email"],
+        },
+      },
+    },
+    async (req, res) => checkUserEmailController(req, res),
+  );
 }
