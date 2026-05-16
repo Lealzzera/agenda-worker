a140609198871c289c66c44d10475309c24c0919
Matheus <matheus.mleall@gmail.com>
2026-05-16 20:36:41 -0300

    feat: removed old registration draft module

--- src/modules/registration-draft/registration-draft.routes.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyInstance } from "fastify";
-import { saveRegistrationDraftController } from "./registration-draft.controller";
-
-export async function registrationDraftRoutes(app: FastifyInstance) {
-  app.post("/", async (req, res) => {
-    return saveRegistrationDraftController(req, res);
-  });
-}
