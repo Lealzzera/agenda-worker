0349845d947b582d966458a42f6a2d87111ec9da
Matheus <matheus.mleall@gmail.com>
2026-06-14 11:39:02 -0300

    added a new migration to remove the canceled status and add some new status

---
+++ prisma/migrations/20260614124030_removed_the_canceled_status_and_added_a_new_ones/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - The values [CANCELED] on the enum `AppointmentStatus` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "AppointmentStatus_new" AS ENUM ('PENDING', 'CONFIRMED', 'NOT_ATTENDED', 'COMPLETED', 'CANCELED_BY_CLINIC', 'CANCELED_BY_PATIENT');
+ALTER TABLE "public"."appointments" ALTER COLUMN "status" DROP DEFAULT;
+ALTER TABLE "appointments" ALTER COLUMN "status" TYPE "AppointmentStatus_new" USING ("status"::text::"AppointmentStatus_new");
+ALTER TYPE "AppointmentStatus" RENAME TO "AppointmentStatus_old";
+ALTER TYPE "AppointmentStatus_new" RENAME TO "AppointmentStatus";
+DROP TYPE "public"."AppointmentStatus_old";
+ALTER TABLE "appointments" ALTER COLUMN "status" SET DEFAULT 'PENDING';
+COMMIT;
